ca453cad089b716f5fbf11111a75dd40d416a505
Tviljan <virallinen@gmail.com>
2024-02-20 09:46:05 +0200

    add tests

---
+++ src/Runner.cs
@@ -0,0 +1,13 @@
+﻿
+public static class Runner
+{
+    public static Task RunAsync()
+    {
+        return Task.Delay(1000);
+    }
+
+    public static bool IsPalindrome(string str)
+    {
+        return str == new string(str.Reverse().ToArray());
+    }
+}
